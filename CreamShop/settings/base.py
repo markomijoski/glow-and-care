dcf0949ddf83272391ef43add240f1449203708e
Marko <marko.mijoski123@gmail.com>
2026-06-30 18:35:09 +0200

    ports modification

--- CreamShop/settings/base.py
+++ CreamShop/settings/base.py
@@ -109,7 +109,7 @@ DATABASES = {
         "NAME":     env("DB_NAME"),
         "USER":     env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
-        "HOST":     env("DB_HOST",     default="localhost"),
+        "HOST":     env("DB_HOST",     default="db"),
         "PORT":     env("DB_PORT",     default="5432"),
         "OPTIONS": {
             # Keep connections alive to reduce per-request overhead
